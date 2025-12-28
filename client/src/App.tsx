f7906b3ca71261d22e0bf123b2632d80ee084ca1
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-12-28 23:02:32 +0900

    wi[

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,13 +1,13 @@
 import type { FC } from "react";
 import { Route, Routes } from "react-router";
 import Home from "./routes/Home";
-import Login from "./routes/Login";
+import Signin from "./routes/Signin";
 
 const App: FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/login" element={<Login />} />
+      <Route path="/signin" element={<Signin />} />
       <Route path="*" element={<h1>404 Not Found</h1>} />
     </Routes>
   );
--- client/src/routes/Signin.tsx
+++ client/src/routes/Signin.tsx
@@ -1,14 +1,14 @@
 import type { FC } from "react";
 import { authClient } from "../lib/auth";
 
-const Login: FC = () => {
-  const handleGitHubLogin = async () => {
+const Signin: FC = () => {
+  const handleGitHubSignin = async () => {
     await authClient.signIn.social({
       provider: "github",
       callbackURL: "http://localhost:5173/",
     });
   };
-  const handleDiscordLogin = async () => {
+  const handleDiscordSignin = async () => {
     await authClient.signIn.social({
       provider: "discord",
       callbackURL: "http://localhost:5173/",
@@ -17,11 +17,11 @@ const Login: FC = () => {
 
   return (
     <div>
-      <h1>Login Page</h1>
-      <button onClick={handleGitHubLogin}>Sign in with GitHub</button>
-      <button onClick={handleDiscordLogin}>Sign in with Discord</button>
+      <h1>Signin Page</h1>
+      <button onClick={handleGitHubSignin}>Sign in with GitHub</button>
+      <button onClick={handleDiscordSignin}>Sign in with Discord</button>
     </div>
   );
 };
 
-export default Login;
+export default Signin;
