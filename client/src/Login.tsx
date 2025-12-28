cafd09f71f24a9cff62182637af0a922b3319178
Myxogastria0808 <r.rstudio.c@gmail.com>
2025-12-28 21:21:24 +0900

    wip

--- client/src/Login.tsx
+++ client/src/Login.tsx
@@ -2,9 +2,15 @@ import type { FC } from "react";
 import { authClient } from "./lib/auth";
 
 const Login: FC = () => {
-  const handleLogin = async () => {
+  const handleGitHubLogin = async () => {
     await authClient.signIn.social({
       provider: "github",
+      callbackURL: "http://localhost:5173/",
+    });
+  };
+  const handleDiscordLogin = async () => {
+    await authClient.signIn.social({
+      provider: "discord",
       callbackURL: "http://localhost:5173/",
     });
   };
@@ -12,7 +18,8 @@ const Login: FC = () => {
   return (
     <div>
       <h1>Login Page</h1>
-      <button onClick={handleLogin}>Sign in with GitHub</button>
+      <button onClick={handleGitHubLogin}>Sign in with GitHub</button>
+      <button onClick={handleDiscordLogin}>Sign in with Discord</button>
     </div>
   );
 };
